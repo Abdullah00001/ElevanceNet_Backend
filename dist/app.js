4bcfd81022937439b0e0ce24e936803c09d02932
Abdullah Bin Omar Chowdhury <abdullahbinomarchowdhury@gmail.com>
2025-02-10 10:47:18 +0600

    chore(setup): logger configgured - winston configured for better logging message - morgan configgured for logging http request middleware Closes #2

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,11 @@
-import express from "express";
+import express from 'express';
+import morgan from 'morgan';
+import { morganMessageFormat, streamConfig } from './configs/morgan.configs.js';
 const app = express();
 app.use(express.json());
+app.use(morgan(morganMessageFormat, {
+    stream: {
+        write: (message) => streamConfig(message),
+    },
+}));
 export default app;
---
+++ dist/configs/morgan.configs.js
@@ -0,0 +1,16 @@
+import logger from './logger.configs.js';
+const morganMessageFormat = '":method :url HTTP/:http-version" :status :res[content-length] :response-time ms';
+const streamConfig = (message) => {
+    const parts = message.match(/"([^"]+)" (\d+) (\d+|-) ([\d.]+ ms)/);
+    if (parts) {
+        const logObject = {
+            method: parts[1].split(' ')[0],
+            url: parts[1].split(' ')[1],
+            status: parts[2],
+            res: parts[3] === '-' ? '0' : parts[3],
+            responseTime: parts[4],
+        };
+        logger.info(`Method: ${logObject.method}, URL: ${logObject.url}, Status: ${logObject.status}, Res: ${logObject.res}, Response Time: ${logObject.responseTime}`);
+    }
+};
+export { morganMessageFormat, streamConfig };
