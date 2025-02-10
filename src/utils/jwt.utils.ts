774b7e00434d866fbda84f19726ae320c6a69bac
Abdullah Bin Omar Chowdhury <abdullahbinomarchowdhury@gmail.com>
2025-02-10 15:56:09 +0600

    feat(auth): add utility for generating and verifying JWT tokens
    - Implemented  function to create JWT tokens with user data.
    - Implemented  function to verify the validity of the JWT token.
    - Added utility for handling authentication with JWT tokens.
    
    Closes #4

---
+++ src/utils/jwt.utils.ts
@@ -0,0 +1,58 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { Payload } from '../interfaces/payload.interfaces.js';
+import {
+  JWT_ACCESS_TOKEN_EXPIRES_IN,
+  JWT_ACCESS_TOKEN_SECRET_KEY,
+  JWT_REFRESH_TOKEN_EXPIRES_IN,
+  JWT_REFRESH_TOKEN_SECRET_KEY,
+} from '../const.js';
+
+export const generateAccessToken = (payload: Payload): string | null => {
+  try {
+    const token = jwt.sign(payload, JWT_ACCESS_TOKEN_SECRET_KEY, {
+      expiresIn: JWT_ACCESS_TOKEN_EXPIRES_IN,
+    });
+    return token;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const generateRefreshToken = (payload: Payload): string | null => {
+  try {
+    const token = jwt.sign(payload, JWT_REFRESH_TOKEN_SECRET_KEY, {
+      expiresIn: JWT_REFRESH_TOKEN_EXPIRES_IN,
+    });
+    return token;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const verifyAccessToken = (token: string): JwtPayload | null => {
+  try {
+    const decoded = jwt.verify(
+      token,
+      JWT_ACCESS_TOKEN_SECRET_KEY
+    ) as JwtPayload;
+    return decoded;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const verifyRefreshToken = (token: string): JwtPayload | null => {
+  try {
+    const decoded = jwt.verify(
+      token,
+      JWT_REFRESH_TOKEN_SECRET_KEY
+    ) as JwtPayload;
+    return decoded;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
